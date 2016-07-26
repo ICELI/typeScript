3b30e0a7d14d19e23ed31b10a54f04826d39c2b9
ICELI <woticeli@gmail.com>
2016-07-26 13:30:00 +0800

    import Module

--- gulp/dist/main.js
+++ gulp/dist/main.js
@@ -1,4 +1,3 @@
-function hello(compiler) {
-    console.log("Hello from " + compiler);
-}
-hello('TS with Gulp');
+"use strict";
+var greet_1 = require("./greet");
+console.log(greet_1.sayHello("import TS with Gulp"));
